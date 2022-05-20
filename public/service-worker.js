0aceb0b5b07c31687026aa39485af90dacbe18b8
Chris392 <chrisokram@gmail.com>
2022-05-20 20:52:07 +0200

    add first caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,41 @@
-console.log("Yippie")
+var cacheName = "v2";
+var filesToCache = ["/offline.html"]
+
+self.addEventListener("install", fetchEvent => {
+    console.log("SW - Install")
+    fetchEvent.waitUntil(
+        caches
+            .open(cacheName)
+            .then(function(cache) {
+                console.log("installed");
+                return cache.addAll(filesToCache);
+            })
+    )
+})
+
+this.addEventListener('fetch', function (evt) {
+    console.log("SW - fetch")
+    console.log(evt.request)
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request);
+        }).catch(function() {
+            return caches.match("/index.html")
+        })
+    )
+});
+
+self.addEventListener("activate", evt => {
+    console.log("SW - Activate")
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                  console.log('[SW - Remove old cache', key);
+                  return caches.delete(key);
+                }
+              }));
+        })
+    )
+})
+
